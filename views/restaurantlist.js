5abbf9a1c33d275e7333efad313c5ab0851d1056
wesliang <wesliang92@gmail.com>
2017-08-18 16:02:04 -0400

    deleted extraneous files and moved into appropriate folders

--- views/restaurantlist.js
+++ views/restaurantlist.js
@@ -1,52 +1,49 @@
-// //var data = [];
-//
-// var viewModel = function() {
-//   var self = this;
-//
-//   //hold ton of restaurants
-//   self.restaurantList = ko.observableArray([]);
-//
-//   initialRestaurants.forEach(function(e) {
-//     self.restaurantList.push( new Restaurant(e) );
-//     //data.push( new Restaurant(e) );
-//   });
-//
-//   //point to starting item in list
-//   this.currentRestaurant = ko.observable( this.restaurantList()[0] );
-//
-//   //get clickItem and pan to it
-//   this.setCurrentMarker = function(clickedItem) {
-//     self.currentRestaurant(clickedItem);
-//     //console.log(markers[clickedItem.id]);
-//     populateInfoWindow(markers[clickedItem.id], largeInfowindow);
-//     map.panTo(markers[clickedItem.id].position);
-//     // map.setZoom(15);
-//   };
-//
-//   //copy of function in map.js, couldn't figure out how to call this from both without code re-use
-//   function populateInfoWindow(marker, infowindow) {
-//     //Check window not already open
-//     if (infowindow.marker != marker) {
-//
-//       infowindow.marker = marker;
-//       infowindow.setContent('<div class= "marker">' + marker.title + '</div>');
-//
-//       //animation for bounce and cancel after 1.5 seconds
-//       marker.setAnimation(google.maps.Animation.BOUNCE);
-//       setTimeout(function() {
-//         marker.setAnimation(null);
-//       }, 1500);
-//
-//       infowindow.open(map, marker);
-//
-//       //reset zoom on map after closing infowindow
-//       // infowindow.addListener('closeclick', function() {
-//       //   map.setZoom(11);
-//       // });
-//     }
-//   }
-//
-//
-// };
-//
-// ko.applyBindings(new viewModel());
+var initialRestaurants = [
+  {
+    name : 'Garden Fresh',
+    info: 'A Chinese restaurant that specializes in Vegetarian dishes',
+    url: 'gardenfresh.us',
+    rating: '4',
+    location: {lat: 37.4455772, lng: -122.165027},
+  },
+
+  {
+    name: 'Marafuku Ramen',
+    info: 'The newest Ramen shop in Japantown featuring the Hakata Tonkotsu Ramen',
+    url: 'marafukuramen.com',
+    rating: '4.5',
+    location: {lat: 37.78505, lng: -122.4342819},
+  },
+
+  {
+    name: 'Gogi Time',
+    info: 'AYCE Kbbq and Hotpot, fresh food for one low price.',
+    url: 'gogitime.com',
+    rating: '4',
+    location: {lat: 37.8155272, lng: -122.2698097},
+  },
+
+  {
+    name: 'Noori',
+    info: 'Indian, Pakistani and Halal cuisine featuring their specialty Butter Chicken and Spicy Garlic Naan',
+    url: 'noorisf.com',
+    rating: '4',
+    location: {lat: 37.6271474, lng: -122.4132797},
+  },
+
+  {
+    name: 'The Codmother Fish and Chips',
+    info: 'Low-key Fish and Chips shop right near the marina of Pier 39, good food at low prices especially in San Francisco.',
+    url: 'codmother.com',
+    rating: '4.5',
+    location: {lat: 37.8072766, lng: -122.4192702},
+  },
+
+  {
+    name: 'Fish',
+    info: 'Specializing in local seafood fare, this is a staple in San Franciscan cuisine with its view of the Bay.',
+    url: '331fish.com',
+    rating: '4',
+    location: {lat: 37.8681402, lng: -122.4999625},
+  }
+];
