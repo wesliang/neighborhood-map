5f0880677bab47a99ea4e2194c1bfee3db64ee87
wesliang <wesliang92@gmail.com>
2017-08-21 16:15:49 -0400

    removed extraneous files and moved features into app.js and ran IIFE on google maps

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+//map object for google maps
+var map;
+//global infowindow to prevent multiple windows from being created
+var infoWindow;
+
 //restaurant objects to initialize
 var initialRestaurants = [
   {
@@ -37,13 +42,25 @@ var initialRestaurants = [
   }
 ];
 
-//global infowindow to prevent multiple windows from being created
-var infoWindow;
+//IIFE to init map after making viewmodel
+(function initMap(){
+  map = new google.maps.Map(document.getElementById('map'), {
+   center: { //center around San Francisco
+     lat: 37.6773748,
+     lng: -122.4170011
+   },
+   zoom: 11,
+   mapTypeId: 'terrain',
+   //properties to style map, sourced from snazzy maps author Diana Caballero https://snazzymaps.com/style/93/lost-in-the-desert
+   styles: [{"elementType":"labels","stylers":[{"visibility":"off"},{"color":"#000000"}]},{"featureType":"landscape","stylers":[{"color":"#f9ddc5"},{"lightness":-7}]},{"featureType":"road","stylers":[{"color":"#813033"},{"lightness":43}]},{"featureType":"poi.business","stylers":[{"color":"#645c20"},{"lightness":38}]},{"featureType":"water","stylers":[{"color":"#1994bf"},{"saturation":-69},{"gamma":0.99},{"lightness":43}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#f19f53"},{"weight":1.3},{"visibility":"on"},{"lightness":16}]},{"featureType":"poi.business"},{"featureType":"poi.park","stylers":[{"color":"#645c20"},{"lightness":39}]},{"featureType":"poi.school","stylers":[{"color":"#a95521"},{"lightness":35}]},{},{"featureType":"poi.medical","elementType":"geometry.fill","stylers":[{"color":"#813033"},{"lightness":38},{"visibility":"off"}]},{},{},{},{},{},{},{},{},{},{},{},{"elementType":"labels"},{"featureType":"poi.sports_complex","stylers":[{"color":"#9e5916"},{"lightness":32}]},{},{"featureType":"poi.government","stylers":[{"color":"#9e5916"},{"lightness":46}]},{"featureType":"transit.station","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","stylers":[{"color":"#813033"},{"lightness":22}]},{"featureType":"transit","stylers":[{"lightness":38}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#f19f53"},{"lightness":-10}]},{},{},{}]
+  });
+}());
 
 //restaurant and its properties and attached markers
 var Restaurant = function(data) {
   //reference to original object of 'this,' in this case it is the 6 restaurant objects
   var self = this;
+
   infoWindow = new google.maps.InfoWindow();
 
   //initialize object fields
@@ -66,10 +83,10 @@ var Restaurant = function(data) {
     //capture id, contact, location, categories etc into results
 		var results = json.response.venues[0];
 
-    //assign objects from JSON to values in our object
+    //assign objects from JSON to values in our object / check to see if undefined first
     self.siteURL = results.url === undefined ? '' : results.url;
-		self.street = results.location.formattedAddress[0];
-    self.city = results.location.formattedAddress[1];
+		self.street = results.location.formattedAddress[0] === undefined ? '' :results.location.formattedAddress[0];
+    self.city = results.location.formattedAddress[1] === undefined ? '': results.location.formattedAddress[1];
 	}).fail(function() {
 		alert('Error with retrieving data from Foursquare. Please refresh the page and try again.');
 	});
@@ -108,4 +125,48 @@ var Restaurant = function(data) {
     map.panTo(this.position);
     infoWindow.open(map, this);
 	});
+};
+
+//let map be global to get accessed by other funcs
+var viewModel = function() {
+  //reference to the original 'this', aka viewModel object
+  var self = this;
+
+  //hold query and restaurants
+  this.restaurantList = ko.observableArray([]);
+  this.query = ko.observable('');
+
+  initialRestaurants.forEach(function(e) {
+    self.restaurantList.push( new Restaurant(e) );
+  });
+
+  this.listClick = function(){
+    google.maps.event.trigger(this.marker, 'click');
+  };
+
+  //filter the list
+  this.filtered = ko.computed( function() {
+    //check if filter exists
+    var filter = self.query().toLowerCase();
+  		if (!filter) {
+  			self.restaurantList().forEach(function(item){
+          //if filter doesn't exist iterate and show all on map
+  				item.show(true);
+  			});
+  			return self.restaurantList();
+  		} else {
+        //pass list into utility function
+  			return ko.utils.arrayFilter(self.restaurantList(), function(item) {
+          var name = item.name.toLowerCase();
+          var result = (name.includes(filter));
+  				item.show(result);
+          return result;
+  			});
+  		}
+  	});
+};
+ko.applyBindings(new viewModel());
+
+var errorHandler = function () {
+  alert("Error could not load map.");
 };
