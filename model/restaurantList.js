b629a18c5f2148cbb6621063b13cbab978d76a1e
wesliang <wesliang92@gmail.com>
2017-08-21 14:35:42 -0400

    moved restaurantList.js to the model folder

---
+++ model/restaurantList.js
@@ -0,0 +1,37 @@
+// var initialRestaurants = [
+//   {
+//     name : 'Garden Fresh',
+//     info: 'A Chinese restaurant that specializes in Vegetarian dishes',
+//     location: {lat: 37.4455772, lng: -122.165027},
+//   },
+//
+//   {
+//     name: 'Marafuku Ramen',
+//     info: 'The newest Ramen shop in Japantown featuring the Hakata Tonkotsu Ramen',
+//     location: {lat: 37.78505, lng: -122.4342819},
+//   },
+//
+//   {
+//     name: 'Gogi Time',
+//     info: 'AYCE Kbbq and Hotpot, fresh food for one low price.',
+//     location: {lat: 37.8155272, lng: -122.2698097},
+//   },
+//
+//   {
+//     name: 'Noori',
+//     info: 'Indian, Pakistani and Halal cuisine featuring their specialty Butter Chicken and Spicy Garlic Naan',
+//     location: {lat: 37.6271474, lng: -122.4132797},
+//   },
+//
+//   {
+//     name: 'The Codmother Fish and Chips',
+//     info: 'Low-key Fish and Chips shop right near the marina of Pier 39, good food at low prices especially in San Francisco.',
+//     location: {lat: 37.8072766, lng: -122.4192702},
+//   },
+//
+//   {
+//     name: 'Fish',
+//     info: 'Specializing in local seafood fare, this is a staple in San Franciscan cuisine with its view of the Bay.',
+//     location: {lat: 37.8681402, lng: -122.4999625},
+//   }
+// ];
