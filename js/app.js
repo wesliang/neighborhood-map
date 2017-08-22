e62cc7a7dda2347b28384560db356b55e089095c
wesliang <wesliang92@gmail.com>
2017-08-22 09:02:04 -0400

    separated objects into restaurant list, added async capabilities

--- js/app.js
+++ js/app.js
@@ -3,47 +3,8 @@ var map;
 //global infowindow to prevent multiple windows from being created
 var infoWindow;
 
-//restaurant objects to initialize
-var initialRestaurants = [
-  {
-    name : 'Garden Fresh',
-    info: 'A Chinese restaurant that specializes in Vegetarian dishes',
-    location: {lat: 37.4455772, lng: -122.165027},
-  },
-
-  {
-    name: 'Marafuku Ramen',
-    info: 'The newest Ramen shop in Japantown featuring the Hakata Tonkotsu Ramen',
-    location: {lat: 37.78505, lng: -122.4342819},
-  },
-
-  {
-    name: 'Gogi Time',
-    info: 'AYCE Kbbq and Hotpot, fresh food for one low price.',
-    location: {lat: 37.8155272, lng: -122.2698097},
-  },
-
-  {
-    name: 'Noori',
-    info: 'Indian, Pakistani and Halal cuisine featuring their specialty Butter Chicken and Spicy Garlic Naan',
-    location: {lat: 37.6271474, lng: -122.4132797},
-  },
-
-  {
-    name: 'The Codmother Fish and Chips',
-    info: 'Low-key Fish and Chips shop right near the marina of Pier 39, good food at low prices especially in San Francisco.',
-    location: {lat: 37.8072766, lng: -122.4192702},
-  },
-
-  {
-    name: 'Fish',
-    info: 'Specializing in local seafood fare, this is a staple in San Franciscan cuisine with its view of the Bay.',
-    location: {lat: 37.8681402, lng: -122.4999625},
-  }
-];
-
-//IIFE to init map after making viewmodel
-(function initMap(){
+// IIFE to init map after making viewmodel
+function initMap(){
   map = new google.maps.Map(document.getElementById('map'), {
    center: { //center around San Francisco
      lat: 37.6773748,
@@ -54,7 +15,9 @@ var initialRestaurants = [
    //properties to style map, sourced from snazzy maps author Diana Caballero https://snazzymaps.com/style/93/lost-in-the-desert
    styles: [{"elementType":"labels","stylers":[{"visibility":"off"},{"color":"#000000"}]},{"featureType":"landscape","stylers":[{"color":"#f9ddc5"},{"lightness":-7}]},{"featureType":"road","stylers":[{"color":"#813033"},{"lightness":43}]},{"featureType":"poi.business","stylers":[{"color":"#645c20"},{"lightness":38}]},{"featureType":"water","stylers":[{"color":"#1994bf"},{"saturation":-69},{"gamma":0.99},{"lightness":43}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"#f19f53"},{"weight":1.3},{"visibility":"on"},{"lightness":16}]},{"featureType":"poi.business"},{"featureType":"poi.park","stylers":[{"color":"#645c20"},{"lightness":39}]},{"featureType":"poi.school","stylers":[{"color":"#a95521"},{"lightness":35}]},{},{"featureType":"poi.medical","elementType":"geometry.fill","stylers":[{"color":"#813033"},{"lightness":38},{"visibility":"off"}]},{},{},{},{},{},{},{},{},{},{},{},{"elementType":"labels"},{"featureType":"poi.sports_complex","stylers":[{"color":"#9e5916"},{"lightness":32}]},{},{"featureType":"poi.government","stylers":[{"color":"#9e5916"},{"lightness":46}]},{"featureType":"transit.station","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","stylers":[{"color":"#813033"},{"lightness":22}]},{"featureType":"transit","stylers":[{"lightness":38}]},{"featureType":"road.local","elementType":"geometry.stroke","stylers":[{"color":"#f19f53"},{"lightness":-10}]},{},{},{}]
   });
-}());
+
+  ko.applyBindings(new ViewModel());
+}
 
 //restaurant and its properties and attached markers
 var Restaurant = function(data) {
@@ -110,7 +73,7 @@ var Restaurant = function(data) {
   this.marker.addListener('click', function(){
     //set content of infowindow
 		self.contentString = '<div><b>' + data.name + "</b></div>" +
-                        '<div"><a href=""' + self.siteURL +'">' + self.siteURL + '</a></div>' +
+                        '<div"><a href="' + self.siteURL +'">' + self.siteURL + '</a></div>' +
                         '<div>' + self.street + '</div>' +
                         '<div>' + self.city + '</div>';
     infoWindow.setContent(self.contentString);
@@ -128,7 +91,7 @@ var Restaurant = function(data) {
 };
 
 //let map be global to get accessed by other funcs
-var viewModel = function() {
+var ViewModel = function() {
   //reference to the original 'this', aka viewModel object
   var self = this;
 
@@ -165,7 +128,6 @@ var viewModel = function() {
   		}
   	});
 };
-ko.applyBindings(new viewModel());
 
 var errorHandler = function () {
   alert("Error could not load map.");
